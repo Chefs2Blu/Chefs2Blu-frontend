d09d2627df12c458f5cac378e10030e038d252c9
Rafael-RD <rafaelreinertd@hotmail.com>
2023-08-02 16:01:23 -0300

    update modelo receita

---
+++ script.js
@@ -0,0 +1,27 @@
+const categorias = {
+    pratoPrincipal: "Prato Principal",
+    bebida: "Bebida",
+    sobremesa: "Sobremesa"
+}
+
+const todasReceitas = [
+    {
+        id: 1,
+        name: "bolo de morango",
+        image: "url",
+        ingredients: ["morangos", "trigo",],
+        instructions: ["junta tudo e mexe",],
+        tips: ["cuida pra n mexer por horas",],
+        category: categorias.sobremesa
+    },
+    {
+        id: 2,
+        name: "Pudim de leite condensado",
+        image: "url",
+        ingredients: ["1 lata de leite condensado", "1 lata de leite (medida da lata de leite condensado)", "3 ovos inteiros", "1 xícara (chá) de açúcar", "1/2 xícara de água"],
+        instructions: ["Pudim", "Primeiro, bata bem os ovos no liquidificador.", "Acrescente o leite condensado e o leite, e bata novamente.", "Calda", "Derreta o açúcar na panela até ficar moreno, acrescente a água e deixe engrossar.", "Coloque em uma forma redonda e despeje a massa do pudim por cima.", "Asse em forno médio por 45 minutos, com a assadeira redonda dentro de uma maior com água.", "Espete um garfo para ver se está bem assado.", "Deixe esfriar e desenforme."],
+        tips: ["cuida pra n mexer por horas",],
+        category: categorias.sobremesa
+    },
+
+]
