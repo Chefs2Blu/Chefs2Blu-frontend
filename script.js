8f9ef4bfae1182f8808b2534185c9d415511e294
Rafael-RD <rafaelreinertd@hotmail.com>
2023-08-02 15:17:59 -0300

    added fonts, reset

---
+++ script.js
@@ -0,0 +1,11 @@
+const todasReceitas = [
+    {
+        id: 1,
+        name: "bolo de morango",
+        ingredients: ["morangos", "trigo",],
+        instructions: ["junta tudo e mexe",],
+        tips: ["cuida pra n mexer por horas",],
+        image: "url",
+    },
+    
+]
