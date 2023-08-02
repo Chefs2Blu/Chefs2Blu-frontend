cb68e0952581d45ed9f938604010ac83653d2c1a
Labes <gdlabes8@gmail.com>
2023-08-02 17:16:45 -0300

    Chef page created

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ const todasReceitas = [
     },
     
 ]
+
+
+const chefs = document.querySelectorAll('.chef')
+const chefsDesc = document.querySelectorAll('.chef-text')
+
+chefs.forEach((chef) => {
+    chef.addEventListener('click', () => {
+        chefsDesc.forEach(desc => {
+            if(desc.id === chef.id){
+               desc.classList.toggle('text-ative')
+            }
+            else{
+                if(desc.classList.contains('text-ative')){
+                    desc.classList.toggle('text-ative')
+                }
+            }
+        })
+    })
+})
